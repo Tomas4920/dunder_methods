bd96a5204a7623debe1a965b38522d8a0a1f78df
Tomas4920 <70300546+Tomas4920@users.noreply.github.com>
2023-09-12 20:25:27 -0500

    Update Clase_Elemento.py

--- Clase_Elemento.py
+++ Clase_Elemento.py
@@ -1,1 +1,17 @@
+from dataclasses import dataclass
 
+@dataclass
+class Elemento:
+    nombre: str
+
+    def __eq__(self, otro):
+        if isinstance(otro, Elemento):
+            return self.nombre == otro.nombre
+        return False
+
+elemento1 = Elemento( " Consola ")
+elemento2 = Elemento( " Terminal ")
+elemento3 = Elemento( " Consola ")
+
+print(elemento1 == elemento2)  
+print(elemento1 == elemento3)  
